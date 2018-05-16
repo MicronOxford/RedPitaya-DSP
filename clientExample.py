13479fb93f319ce6338afb7524dd5845ba28426e
Tiago Susano Pinto <tiagosusanopinto@gmail.com>
2018-05-16 14:08:38 +0100

    Remove semicolons from python code
    Also remove unused python packages from makefile

--- clientExample.py
+++ clientExample.py
@@ -1,28 +1,28 @@
 import Pyro4
 
-dspDemo = False;
+dspDemo = False
 
-dspURI = 'PYRO:pyroDSP@10.42.0.175:7000';
+dspURI = 'PYRO:pyroDSP@10.42.0.175:7000'
 
-timeTest = [1, 2, 3];
-pinsTest = [-1,-2,11];
-valsTest = [9191, -4000, -3];
+timeTest = [1, 2, 3]
+pinsTest = [-1,-2,11]
+valsTest = [9191, -4000, -3]
 
-demoIntervalsOfTime = 100 # microseconds (1e-6 sec) 
+demoIntervalsOfTime = 100 # microseconds (1e-6 sec)
 
 try:
-    pyroDSP = Pyro4.Proxy(dspURI);
+    pyroDSP = Pyro4.Proxy(dspURI)
 
     if dspDemo:
-        print('starting demo');
-        pyroDSP.demo(demoIntervalsOfTime);
+        print('starting demo')
+        pyroDSP.demo(demoIntervalsOfTime)
     else:
-        print('creating action table file');
-        pyroDSP.setProfile(timeTest, pinsTest, valsTest);
-        pyroDSP.DownloadProfile();
-        print('staring dsp');
-        pyroDSP.trigCollect();
+        print('creating action table file')
+        pyroDSP.setProfile(timeTest, pinsTest, valsTest)
+        pyroDSP.DownloadProfile()
+        print('staring dsp')
+        pyroDSP.trigCollect()
 
-    print('end');
+    print('end')
 except Exception as e:
-    print(e);
+    print(e)
